deb6c6758d88b03913b7226f680f6eb3ebf844a9
afwan-belang <muhammadhaikalafwan@gmail.com>
2026-03-16 13:59:17 +0700

    passing paramter
    hidup jokowi

--- main.js
+++ main.js
@@ -3,3 +3,9 @@ const namaOrangTua = ['Wawan', 'Ajat', 'Saiul', 'dodo']
 for(let i = 0; i < namaOrangTua.length; i++){
   console.log(namaOrangTua[i]
 }
+
+const gantiNama = (params) => {
+  params ? console.log('parameternya ada', params) : console.log('paramternya tidak ada');
+}
+
+gantiNama('hidup jokowi');
